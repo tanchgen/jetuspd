4774afbaa438ab6c60b116c279afc441f3c9c08b
Gennady Tanchin <g.tanchin@yandex.ru>
2021-11-14 12:41:54 +0300

    .

--- src/main.c
+++ src/main.c
@@ -27,7 +27,13 @@
 #include "main.h"
 
 #include "isens.h"
-
+//#include "gsm.h"
+//#include "adc.h"
+//#include "dma.h"
+//#include "rtc.h"
+//#include "usart.h"
+#include "stm32l1xx_ll_iwdg.h"
+#include "stm32l1xx_ll_rcc.h"
 #include "MQTTSim800.h"
 /* USER CODE END Includes */
 
@@ -61,6 +67,9 @@ RCC_ClocksTypeDef RCC_Clocks;
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
 void adcProcess( void );
+
+void Configure_IWDG(void);
+void Check_IWDG_Reset(void);
 
 /* USER CODE END PFP */
 
@@ -103,6 +112,10 @@ int main(void)
 
     gsmRunPhase = PHASE_NON;
 
+    // Запускаем watchdog на случай зависания прошивки
+//    Check_IWDG_Reset();
+//    Configure_IWDG();
+
 //    SIM800.mqttReceive.mqttData = simHnd.rxh->rxFrame;
 //    gsmState = GSM_WORK;
 //    SIM800.mqttServer.mqttconn = SET;
@@ -112,6 +125,8 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
     while (1) {
+      /* Refresh IWDG down-counter to default value */
+      LL_IWDG_ReloadCounter(IWDG);
       timersClock();
       adcProcess();
       isensProcess();
@@ -125,6 +140,54 @@ int main(void)
 }
 
 /* USER CODE BEGIN 4 */
+/**
+* @brief  This function configures IWDG
+* @param  None
+* @retval None
+*/
+void Configure_IWDG(void){
+  /* Enable the peripheral clock of DBG register (uncomment for debug purpose) */
+  /* ------------------------------------------------------------------------- */
+  /*  LL_DBGMCU_APB1_GRP1_FreezePeriph(LL_DBGMCU_APB1_GRP1_IWDG_STOP); */
+
+  /* Enable the peripheral clock IWDG */
+  /* -------------------------------- */
+  LL_RCC_LSI_Enable();
+  while (LL_RCC_LSI_IsReady() != 1)
+  {}
+
+  /* Configure the IWDG with window option disabled */
+  /* ------------------------------------------------------- */
+  /* (1) Enable the IWDG by writing 0x0000 CCCC in the IWDG_KR register */
+  /* (2) Enable register access by writing 0x0000 5555 in the IWDG_KR register */
+  /* (3) Write the IWDG prescaler by programming IWDG_PR from 0 to 7 - LL_IWDG_PRESCALER_4 (0) is lowest divider*/
+  /* (4) Write the reload register (IWDG_RLR) */
+  /* (5) Wait for the registers to be updated (IWDG_SR = 0x0000 0000) */
+  /* (6) Refresh the counter value with IWDG_RLR (IWDG_KR = 0x0000 AAAA) */
+  LL_IWDG_Enable(IWDG);                             /* (1) */
+  LL_IWDG_EnableWriteAccess(IWDG);                  /* (2) */
+  LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_32);  /* (3) */
+  LL_IWDG_SetReloadCounter(IWDG, 0xFEE);            /* (4) */
+  while (LL_IWDG_IsReady(IWDG) != 1)                /* (5) */
+  {}
+  LL_IWDG_ReloadCounter(IWDG);                      /* (6) */
+  DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_IWDG_STOP;
+}
+
+
+/**
+  * @brief  This function check if the system has resumed from IWDG reset
+  * @param  None
+  * @retval None
+  */
+void Check_IWDG_Reset(void)
+{
+  if (LL_RCC_IsActiveFlag_IWDGRST())
+  {
+    /* clear IWDG reset flag */
+    LL_RCC_ClearResetFlags();
+  }
+}
 
 /* USER CODE END 4 */
 
